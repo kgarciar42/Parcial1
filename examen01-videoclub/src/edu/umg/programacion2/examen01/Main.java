7baecf9ecb559d8e42e5f71af7276192b6e7ea6a
Kervin Garcia <kgarciar42@miumg.edu.gt>
2026-08-07 19:41:49 -0600

    prueba de enlace

--- examen01-videoclub/src/edu/umg/programacion2/examen01/Main.java
+++ examen01-videoclub/src/edu/umg/programacion2/examen01/Main.java
@@ -50,7 +50,7 @@ public class Main {
 		System.out.println("0) Salir");
 		System.out.print("Opción: ");
 	}
-
+	//Prueba para verificar que esta enlazado//
 	private static void listarTodos(Videoclub videoclub) {
 		for (Pelicula pelicula : videoclub.listarTodos()) {
 			System.out.println(pelicula);
